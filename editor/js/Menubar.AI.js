8d8eaf9f7486cbcdfd63bb55c710cb54a0e7f980
ForGoodTech <88217240+ForGoodTech@users.noreply.github.com>
2024-12-09 00:54:34 -0500

    index.php Menubar.AI.js

--- editor/js/Menubar.AI.js
+++ editor/js/Menubar.AI.js
@@ -1,5 +1,28 @@
 import { UIPanel } from './libs/ui.js';
 
+function start_ai_window()
+{
+    const url = '../editor/ext/ai/this_ai.html'; // URL of the web app
+    const newWindow = window.open(
+        url,
+        '_blank',
+        'width=1024,height=800,left=480,top=100'
+    );
+
+    if (newWindow) 
+    {
+        // Optional: Set up a function to be called when the new window is ready
+        newWindow.onload = function() {
+            // This code will run once the new window has fully loaded
+            // Example: You can directly access the new window's DOM or JavaScript functions
+        };
+    } 
+    else 
+    {
+        alert('Popup blocked. Please allow popups for this site.');
+    }
+}
+
 function MenubarAI( editor ) {
 
 	const container = new UIPanel();
@@ -10,6 +33,7 @@ function MenubarAI( editor ) {
 	title.setTextContent( 'AI' );
 	title.onClick( function () 
         {
+            start_ai_window();
             console.log('Start AI');
         } 
     );
